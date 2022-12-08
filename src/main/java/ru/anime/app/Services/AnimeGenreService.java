10591d57b9851e6e335276f54a93ff1eecd5d865
li3osoma <liza031008@mail.ru>
2022-12-08 17:08:42 +0300

    всё!

--- src/main/java/ru/anime/app/Services/AnimeGenreService.java
+++ src/main/java/ru/anime/app/Services/AnimeGenreService.java
@@ -2,12 +2,12 @@ package ru.anime.app.Services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import ru.anime.app.Models.Anime;
 import ru.anime.app.Models.AnimeGenre;
 import ru.anime.app.Models.Genre;
 import ru.anime.app.Reposits.AnimeGenreRepository;
@@ -31,8 +31,8 @@ public class AnimeGenreService {
     public List<AnimeGenre> getAnimeGenreListByAnimeAndGenre(String animeTitle,String genreName){
         List<AnimeGenre> animeGenreList;
         animeGenreList=animeGenreRepository.findAll().stream().filter(animeGenre ->
-                animeGenre.getGenre().getName().equalsIgnoreCase(genreName) &&
-                animeGenre.getAnime().getTitle().toLowerCase().contains(animeTitle.toLowerCase())).toList();
+                animeGenre.getGenre().getName().toLowerCase().equals(genreName.toLowerCase()) &&
+                        animeGenre.getAnime().getTitle().toLowerCase().contains(animeTitle.toLowerCase())).toList();
         return animeGenreList;
     }
 
@@ -41,16 +41,4 @@ public class AnimeGenreService {
         Genre genre=genreRepository.findGenreByName(genre_name);
         return animeGenreRepository.findAllByGenre(genre, pageable);
     }
-
-//    public void update(Anime anime, Genre genre){
-//        AnimeGenre animeGenre= new AnimeGenre(anime,genre);
-//        AnimeGenre animeGenre1=animeGenreRepository.findAnimeGenreByAnimeAndGenre(anime,genre);
-//        if(animeGenre1==null) animeGenreRepository.save(animeGenre);
-//        else{
-//            animeGenreRepository.delete(animeGenre1);
-//            animeGenreRepository.save(animeGenre);
-//        }
-//    }
-
-
 }
